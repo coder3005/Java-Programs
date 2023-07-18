ba371e8c221d14ee9e46d8017470e588552c6e7e
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2023-07-18 17:04:49 +0530

    Create For.java

---
+++ For.java
@@ -0,0 +1,14 @@
+/* program */
+class For
+{
+    public static void main(String[] args) {
+
+        int i=1;
+        System.out.println(i + " ");
+        ++i;
+        System.out.println(i + " ");
+        ++i;
+        System.out.println(i + " ");
+        ++i;
+    }
+}
