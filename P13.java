7212cae2a5b9eb083dba09babb4dcadba8f11f25
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2023-07-19 12:44:02 +0530

    Create P13.java

---
+++ P13.java
@@ -0,0 +1,23 @@
+/* Vowel or Consonant in Java */
+// input a e i o u ---> vowel
+// input b c d etc...
+import java.util.Scanner;
+class VowelConsonant
+{
+    public static void main(String[] args) {
+
+        char ch;
+        System.out.print("Enter any Character ");
+        Scanner r=new Scanner(System.in);
+        ch=r.next().charAt(0);
+
+        if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u')
+        {
+            System.out.print("Vowel");
+        }
+        else
+        {
+            System.out.print("Consonant");
+        }
+    }
+}
