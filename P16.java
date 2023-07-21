a3db0a3d85d592487dcd8633046ed685da3a9785
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2023-07-21 12:27:50 +0530

    Create P16.java

---
+++ P16.java
@@ -0,0 +1,25 @@
+/* Find greatest number among three Numbers */
+// input   10  20  30  ---> 30
+
+class Greatest 
+{
+    public static void main(String[] args) {
+
+        int a=10,b=20,c=30;
+
+        if(a>b)
+        {
+            if(a>c)
+            System.out.print(a);
+            else
+            System.out.print(c);
+        }
+        else 
+        {
+            if(b>c)
+            System.out.print(b);
+            else 
+            System.out.print(c);
+        }
+    }
+}
