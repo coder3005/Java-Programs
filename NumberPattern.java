dde73de0dfcbcf129db8c37a9dc3ccba50562bfd
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2023-07-26 14:19:20 +0530

    Create NumberPattern.java

---
+++ NumberPattern.java
@@ -0,0 +1,16 @@
+/* Print Number Pattern */
+class A 
+{
+    public static void main(String[] args) {
+        
+        int i,j;
+        for(i=1;i<=5;i++) // rows
+        {
+            for(j=1;j<=i;j++)  // column
+            {
+                System.out.print(j);
+            }
+            System.out.print("\n");
+        }
+    }
+}
