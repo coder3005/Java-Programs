fcb5fa54036bd23c84825e1765b46701d2a1f6b0
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2023-07-26 11:18:11 +0530

    Create StarPattern2.java

---
+++ StarPattern2.java
@@ -0,0 +1,20 @@
+/* Star Pattern Program */
+class A 
+{
+    public static void main(String[] args) {
+        
+        int i,j,k;
+        for(i=1;i<=5;i++)  // rows
+        {
+            for(j=i;j<5;j++)  // space
+            {
+                System.out.print(" ");
+            }
+            for(k=1;k<=i;k++)  // column
+            {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+    }
+}
