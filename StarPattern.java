cb3ff4595fed0b1626acfdab4737d92e4752c784
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2023-07-26 11:13:31 +0530

    Update StarPattern.java

--- StarPattern.java
+++ StarPattern.java
@@ -4,9 +4,9 @@ class A
     public static void main(String[] args) {
         
         int i,j;
-        for(i=1;i<=5;i++)
+        for(i=1;i<=5;i++)  // rows
         {
-            for(j=1;j<=i;j++)
+            for(j=1;j<=i;j++)  // columns
             {
                 System.out.print("*");
             }
