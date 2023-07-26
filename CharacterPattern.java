d7cadcff43d7050a08e47f3220c1f2a6c2d663ef
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2023-07-26 14:30:37 +0530

    Update CharacterPattern.java

--- CharacterPattern.java
+++ CharacterPattern.java
@@ -4,7 +4,7 @@ class A
     public static void main(String[] args) {
         
         char i,j;
-        for(i='A';i<='E';i++)  // rows
+        for(i='A';i<='E';i++)  // rows, in place of A and E we can write 65 and 69 respectively, will give same result
         {
             for(j='A';j<=i;j++)  // column
             {
