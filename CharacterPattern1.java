d33c48538d657d6be3b601f37e3b717756875022
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2023-07-26 14:33:04 +0530

    Create CharacterPattern1.java

---
+++ CharacterPattern1.java
@@ -0,0 +1,16 @@
+/* Print Character  Pattern */
+class A 
+{
+    public static void main(String[] args) {
+        
+        char i,j;
+        for(i='A';i<='E';i++)
+        {
+            for(j='A';j<=i;j++)
+            {
+                System.out.print(i);
+            }
+            System.out.print("\n");
+        }
+    }
+}
